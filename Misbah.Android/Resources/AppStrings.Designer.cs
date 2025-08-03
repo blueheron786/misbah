3ce093df1ce2efa417f8a8828a6a632f2600db7b
blueheron786 <blueheron786@gmail.com>
2025-08-03 09:18:42 -0400

    Refactor: rename
    Misbah.Desktop, Misbah.Android makes mroe sense.

---
+++ Misbah.Android/Resources/AppStrings.Designer.cs
@@ -0,0 +1,41 @@
+// Auto-generated for localization
+namespace Misbah.Android.Resources {
+    using System;
+    using System.Reflection;
+    using System.Resources;
+    using System.Globalization;
+
+    public class AppStrings {
+        private static ResourceManager resourceMan;
+        private static CultureInfo resourceCulture;
+
+        public static ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
+                    resourceMan = new ResourceManager("Misbah.Web.Resources.AppStrings", typeof(AppStrings).Assembly);
+                }
+                return resourceMan;
+            }
+        }
+
+        public static CultureInfo Culture {
+            get { return resourceCulture; }
+            set { resourceCulture = value; }
+        }
+
+        public static string GitHubRepo => ResourceManager.GetString("GitHubRepo", resourceCulture);
+        public static string WelcomeToMisbah => ResourceManager.GetString("WelcomeToMisbah", resourceCulture);
+        public static string NoHubLoaded => ResourceManager.GetString("NoHubLoaded", resourceCulture);
+        public static string LoadHub => ResourceManager.GetString("LoadHub", resourceCulture);
+        public static string HideFolders => ResourceManager.GetString("HideFolders", resourceCulture);
+        public static string ShowFolders => ResourceManager.GetString("ShowFolders", resourceCulture);
+        public static string Notes => ResourceManager.GetString("Notes", resourceCulture);
+        public static string NoNotesFound => ResourceManager.GetString("NoNotesFound", resourceCulture);
+        public static string Preview => ResourceManager.GetString("Preview", resourceCulture);
+        public static string Edit => ResourceManager.GetString("Edit", resourceCulture);
+        public static string Save => ResourceManager.GetString("Save", resourceCulture);
+        public static string SelectNoteToViewOrEdit => ResourceManager.GetString("SelectNoteToViewOrEdit", resourceCulture);
+        public static string NoFoldersFound => ResourceManager.GetString("NoFoldersFound", resourceCulture);
+        public static string Cancel => ResourceManager.GetString("Cancel", resourceCulture);
+    }
+}
---
+++ Misbah.Desktop/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using BlazorDesktop.Hosting;
+using Misbah.Desktop.Components;
+using Misbah.Core.Services;
+using Misbah.Core.Utils;
+
+var builder = BlazorDesktopHostBuilder.CreateDefault(args);
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.RootComponents.Add<Routes>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+if (builder.HostEnvironment.IsDevelopment())
+{
+    builder.UseDeveloperTools();
+}
+
+
+// Register services for DI
+builder.Services.AddSingleton<INoteService>(sp => new NoteService("Notes")); // TODO: set actual notes root path
+builder.Services.AddSingleton<IFolderService, FolderService>();
+builder.Services.AddSingleton<SearchService>();
+builder.Services.AddSingleton<MarkdownRenderer>();
+
+await builder.Build().RunAsync();
