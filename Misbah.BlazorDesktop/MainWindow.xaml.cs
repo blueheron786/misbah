d3c7f114645fcaae76e4b899465b3045978031b8
Alibhai, Ashiq (SSC SPC) <ashiq.alibhai@ssc-spc.gc.ca>
2025-08-07 13:21:40 -0400

    Fix: app works again lol

--- Misbah.BlazorDesktop/MainWindow.xaml.cs
+++ Misbah.BlazorDesktop/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components.WebView.Wpf;
 using System.Windows;
-using Misbah.BlazorDesktop.Components;
 
 namespace Misbah.BlazorDesktop
 {
@@ -17,11 +16,6 @@ namespace Misbah.BlazorDesktop
             set 
             { 
                 blazorWebView.Services = value;
-                blazorWebView.RootComponents.Add(new RootComponent
-                {
-                    Selector = "#app",
-                    ComponentType = typeof(Misbah.BlazorDesktop.Components.Routes)
-                });
             }
         }
     }
