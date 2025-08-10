4cbe66421301ba826bf2f67f9621e59040387cff
blueheron786 <blueheron786@gmail.com>
2025-08-10 14:35:51 -0400

    Delete everything related to Android

--- Misbah.Android/BuildInfo.cs
+++
@@ -1,8 +0,0 @@
-// This file tracks the build number for the Android app. Increment this value each time a test build is requested.
-namespace Misbah.Android
-{
-    public static class BuildInfo
-    {
-        public static int BuildNumber => 2;
-    }
-}
